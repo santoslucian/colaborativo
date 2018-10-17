040acf5b0b45c52fb676ae9ff63ba150d890aab4
kingtella <43422091+kingtella@users.noreply.github.com>
2018-10-17 16:42:07 -0300

    class Dividir

---
+++ calculadoraoo/calculadoraoo/Dividir.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraoo
+{
+    class Dividir
+    {
+        public Dividir()
+        {
+            Console.WriteLine("Objeto Dividir Criado");
+
+        }
+
+        public Dividir(int num1, int num2)
+        {
+            int total = num1 * num2;
+            Console.WriteLine("Resultado de Dividir: " + total);
+        }
+
+        public void DividirEterno()
+        {
+
+        }
+    }
+}
