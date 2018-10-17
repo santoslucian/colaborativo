2cc33344ccc7e23b7a009245bb507f772bd58f69
santoslucian <43422059+santoslucian@users.noreply.github.com>
2018-10-17 16:42:04 -0300

    Class Soma

---
+++ calculadoraoo/calculadoraoo/Soma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraoo
+{
+    class Soma
+    {
+        public Soma()
+        {
+            Console.WriteLine("objeto Soma Craida.");
+        }
+
+        public Soma(int num1, int num2)
+        {
+            int total = num1 * num2;
+            Console.WriteLine("Resultado da Soma:" + total);
+        }
+
+        public void SomaEterno()
+        {
+
+
+        }
+    }
+}
