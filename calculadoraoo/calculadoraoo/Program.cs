044b04ba89af3de0df4d3c3409b75f115ed7616f
santoslucian <43422059+santoslucian@users.noreply.github.com>
2018-10-17 17:38:56 -0300

    Somar

--- calculadoraoo/calculadoraoo/Program.cs
+++ calculadoraoo/calculadoraoo/Program.cs
@@ -9,7 +9,20 @@ namespace calculadoraoo
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // cria uma variavel da classe Somar.
+            Soma somar;
+
+            //O operador "new" cria um objeto da classe; 
+            somar = new Soma();
+
+            somar.num1 = 5;
+            somar.n2 = 10;
+            somar.total = somar.num1 + somar.n2;
+
+            Console.WriteLine("Primeiro Numero : " + somar.num1);
+            Console.WriteLine("Segundo Numero : " + somar.n2);
+            Console.ReadKey();
         }
     }
 }
--- calculadoraoo/calculadoraoo/Soma.cs
+++ calculadoraoo/calculadoraoo/Soma.cs
@@ -8,15 +8,19 @@ namespace calculadoraoo
 {
     class Soma
     {
+        //Criar os atributos(Caracteristicas)
+        public int num1, n2, total;
         public Soma()
         {
-            Console.WriteLine("objeto Soma Craida.");
+            Console.WriteLine("Objeto Soma Criado.");
         }
 
         public Soma(int num1, int num2)
         {
-            int total = num1 * num2;
-            Console.WriteLine("Resultado da Soma:" + total);
+            this.num1 = num1;
+            this.n2 = num2;
+            this.total = num1 + n2;
+            Console.WriteLine("Resultado da Soma:" + this.total);
         }
 
         public void SomaEterno()
